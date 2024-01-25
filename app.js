2aa670ac925a6c438d346db63a4df2982c7bef67
Muhammad Osama <149009025+usamaanwr@users.noreply.github.com>
2024-01-25 04:49:37 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Asignment 2
+
+var lenght = 30;
+var widht = 20;
+
+var area = (lenght*widht);
+console.log(area)
+
+let userNamne = prompt("Your Name");
+alert("assalam o alaikum .welcome to my page")
+
+console.log("Muhammad Osama");
